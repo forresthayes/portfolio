520ce926c55c0834fdc10ee2441b92586546dbe6
Forrest Hayes <forresthayes@pm.me>
2020-05-24 17:00:55 -0500

    Style Project cards on hover and make them activate modal on click

--- web/src/components/Project/Project.js
+++ web/src/components/Project/Project.js
@@ -5,14 +5,14 @@ import { useState } from 'react'
 const Project = ({ title, cover, ...props }) => {
   const [isActive, setIsActive] = useState(false)
   return (
-    <Card backgroundColor="light">
-      <Card.Content>
+    <Card backgroundColor="light" className="project hvr-box-shadow-outset">
+      <Card.Content onClick={() => setIsActive(true)}>
         <Title size={4}>{title}</Title>
-        <Image.Container onClick={() => setIsActive((state) => !state)}>
+        <Image.Container>
           <Image src={cover} />
         </Image.Container>
-        <ProjectModal {...{ title, isActive, setIsActive, ...props }} />
       </Card.Content>
+      <ProjectModal {...{ title, isActive, setIsActive, ...props }} />
     </Card>
   )
 }
